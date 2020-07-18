4f476a6a196966dfa78fc012c6ad1f7b7189e3af
Michael Johnson <44432163+MichaelDavidJohnson@users.noreply.github.com>
2020-07-18 04:18:00 +0100

    fixed spacing

--- portscan.py
+++ portscan.py
@@ -4,59 +4,39 @@ Created on Mon Jul  6 14:06:24 2020
 
 @author: Lauren Tooth
 """
-
 import socket
 import subprocess
 import sys
 import time
-
-#function to validate port number input
-def inputNumber(message):
+def inputNumber(message): #function to validate port number input
     portNumber = input(message)
     if not portNumber.isdigit():
         return inputNumber("Not an integer! Please input a valid port number: ") #message to alert user that an integer value has not been entered
-       
-    else:
-        #check if port entered is in the range of possible port numbers.
+
+    else: #check if port entered is in the range of possible port numbers.
         if int(portNumber) > 65535 or int(portNumber) < 1:
             return inputNumber("Out of range. Please input a valid port number: ") #message to alert user the number entered is out of range
         else:        
             return int(portNumber) #return valid value
         
-
-#clear screen
-subprocess.call('clear', shell=True)
-
-#ask for input
-print ('Welcome to PyNetScan!')
+subprocess.call('clear', shell=True) #clear screen
+print ('Welcome to PyNetScan!') #ask for input
 remoteServer = input("Enter host to scan: ")
-
-
 remoteServerIP = socket.gethostbyname(remoteServer)
-
-
 #input port numbers
 port1 = inputNumber("Please enter the first port number to scan: ")
 port2 = inputNumber("Please enter the final port number to scan: ")
 
-#while loop to evaluate if the first port number inputted is smaller than the final port number.
-while port1 > port2:
+while port1 > port2: #while loop to evaluate if the first port number inputted is smaller than the final port number.
     print ('First port specified must be smaller than the final port specified. Please try again.') #error message
     port1 = inputNumber("Please enter the first port number to scan: ")
     port2 = inputNumber("Please enter the final port number to scan: ")
 
-
-#print information
-print ("-" * 60)
+print ("-" * 60) #print information
 print ("Please wait, scanning host", remoteServerIP)
 print ("-" *60)
-
-
 port3 = port2 + 1
-
-#check current time
-t1 = time.time()
-
+t1 = time.time() #check current time
 try:
     for port in range (port1, port3): #loop to ensure all ports specified are scanned.
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -69,19 +49,13 @@ try:
 except KeyboardInterrupt: #user presses key on keyboard.
     print ("You cancelled the process.")
     sys.exit()
-
 except socket.gaierror: #if there is an error with the hostname.
     print ("Hostname could not be resolved. Exiting...")
     sys.exit()
-
 except socket.error: #if there is no response from host.
     print ("Couldn't connect to server.")
     sys.exit()
-
-#check time again
-t2 = time.time()
-
+t2 = time.time() #Recover ending time for time calculation.
 total = t2 - t1
-
 #output time taken to perform scan
 print ("Scan completed in: ", total)
